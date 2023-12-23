4a7f1abbcc8fe298fae86915e3d8c396ac298f16
RyanFurt12 <ryanfurtadinho9@gmail.com>
2023-12-23 12:54:00 -0300

    add: hover in cards

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -6,6 +6,7 @@ import { dataFake } from '../../data/dataFake'
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
+
 export class SmallCardComponent implements OnInit {
   @Input()
   Id:number= 0
